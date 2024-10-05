6191750cd6d09e7adeb7efdd0d7e060857eb2288
Bulatik <vaca.hack01@gmail.com>
2024-10-05 12:31:54 +0300

    5.10.2024

--- 17929/17929.py
+++ 17929/17929.py
@@ -1,5 +1,45 @@
-managers = 7
-manager_price = 100
-price = 2700
-person = 22
-print('Прибыль за год:', managers*manager_price+person*price*2)
+# name = input('Имя: ')
+# surname = input('Фамилия: ')
+# age = input('Возраст: ')
+# dep = input('Отдел: ')
+# status = input('Стажировка: ')
+# print('Имя: ', name)
+# print('Фамилия: ', surname)
+# print('Возраст: ', age)
+# print('Отдел: ', dep)
+# print('Стажировка: ', status, '%')
+
+
+# name = input('Введите имя:')
+# surname = input('Введите фамилию')
+# age = input('Введите возраст:')
+# dep = input('Введите отдел:')
+# status = input('На сколько процентов завершена стажировка?')
+# print('Имя:', name)
+# print('Фамилия:', surname)
+# print('Возраст:', age)
+# print('Отдел:', dep)
+# print('Стажировка завершена на:', status)
+# name = input('Введите имя:')
+# project = input('Введите название проекта:')
+# days = input('Сколько дней работал:')
+# days = int(days)
+# hours = input('Сколько часов работал:')
+# hours = int(hours)
+# result = days * 9 + hours
+# print('Работник', name, 'потратил', result, 'часа(ов) на проект', project)
+
+
+# import random
+# import time
+# print('Старт')
+# num = random.randint(1, 10)
+# time.sleep(num)
+# print('Конец, прошло', num, 'секунд')
+
+
+# import random 
+# dice = input('Кол-во граней: ')
+# dice = int(dice)
+# result = random.randint(1, dice)
+# print(result)
