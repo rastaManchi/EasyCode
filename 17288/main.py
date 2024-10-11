0288baaf80909499999c080547d4e6ca537a020d
Bulatik <vaca.hack01@gmail.com>
2024-10-11 19:30:18 +0300

    11.10.2024

--- 17288/main.py
+++ 17288/main.py
@@ -1,21 +1,3 @@
-min_x = -5
-max_x = 5
-min_y = -5
-max_y = 5
-
-for y in range(min_y, max_y+1):
-    for x in range(min_x, max_x+1):
-        print('*', end=' ')
-    print()
-
-
-
-all_damage = 0 
-hp = 100
-
-for i in range(5):
-    hp -= 5
-    all_damage += 5
-
-print('Здоровье', hp)
-print('Полученный урон', all_damage)
+name = input('Введите имя: ')
+review = input('Введите отзыв: ')
+print(f'{name}, спасибо за отзыв!\nВ вашем отзыве {len(review)} символов!')
