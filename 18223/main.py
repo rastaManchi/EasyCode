a57b534048b0a9add913b9563877f469fcfb96dd
Bulatik <vaca.hack01@gmail.com>
2024-10-15 21:41:52 +0300

    15.10.2024

--- 18223/main.py
+++ 18223/main.py
@@ -1,7 +1,28 @@
-import random
+# text = 'Easy Code'
+# print(text[0:4])
+# huge_text = '''Привет
+# Как дела\n
+# Что делаешь'''
+# length = len(text)
+# print('В словосочитании "', text, '"', length, 'символов!')
+# print(f'В словосочитании "{text}" {length} символов!')
 
-dice = input('Сколько граней на кубе? ')
-dice = int(dice)
 
-result = random.randint(1, dice)
-print(result)
+feedback = 'Отличный ресторан! Самые вкусные блюда - макароны с креветками и чеснок с карамели'
+dish1 = feedback[41:62]
+dish2 = feedback[65:82]
+print(f'Любимые блюда: {dish1}, {dish2}')
+
+
+# name = input('Введите ваше имя: ')
+# review = input('Введите отзыв: ')
+# length = len(review)
+# print(f'{name}, спасибо за отзыв!\nВ отзыве {length} символов!')
+
+name = input('Введите ваше имя: ')
+print(name.lower())
+print(name.upper())
+print(name)
+
+
+# print(f'сумма: {int(input())+int(input())+int(input())}')
