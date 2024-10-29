5352362c8c99389f92a48ff4599138e2e694dd1d
Bulatik <vaca.hack01@gmail.com>
2024-10-29 14:27:11 +0300

    28.10.2024

--- 17439/main.py
+++ 17439/main.py
@@ -1,32 +1,10 @@
-moscow_uch = 2399
-moscow_mam = 861
-mam_uch = 1744
+cups = int(input('Сколько чашек: '))
+floor = int(input('Какой этаж: '))
 
-city1 = input('Введите первый город: ')
-
-if city1 == 'мамадыш':
-    city2 = input('Введите второй город: ')
-    if city2 == 'москва':
-        print(f'{moscow_mam}км между {city1} -- {city2}')
-    elif city2 == 'учкудук':
-        print(f'{mam_uch}км между {city1} -- {city2}')
-    elif city2 == city1:
-        print(f'0км между {city1} -- {city2}')
-
-elif city1 == 'учкудук':
-    city2 = input('Введите второй город: ')
-    if city2 == 'москва':
-        print(f'{moscow_uch}км между {city1} -- {city2}')
-    elif city2 == 'мамадыш':
-        print(f'{mam_uch}км между {city1} -- {city2}')
-    elif city2 == city1:
-        print(f'0км между {city1} -- {city2}')
-        
-elif city1 == 'москва':
-    city2 = input('Введите второй город: ')
-    if city2 == 'учкудук':
-        print(f'{moscow_uch}км между {city1} -- {city2}')
-    elif city2 == 'мамадыш':
-        print(f'{moscow_mam}км между {city1} -- {city2}')
-    elif city2 == city1:
-        print(f'0км между {city1} -- {city2}')
+while cups >= 0:
+    if (1<=cups<=3 and floor <= 100) or (4<=cups<=7 and floor <= 50) or (cups>=7 and floor <=2):
+        print('Заказ выполнен!')
+    else:
+        print('Доставку сделать не получится')
+    cups = int(input('Сколько чашек: '))
+    floor = int(input('Какой этаж: '))
