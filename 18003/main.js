1e314ce87b425c4d9c0407181f93b9b9be2c371e
Bulatik <vaca.hack01@gmail.com>
2024-11-02 19:25:30 +0300

    02.11.2024

--- 18003/main.js
+++ 18003/main.js
@@ -1,37 +1,55 @@
 
-let  player = {}
+// let  player = {}
 
-player.name = "Булат";
-player.age = 22;
-player.position = "Holl";
-player.departament = "Web deportament";
+// player.name = "Булат";
+// player.age = 22;
+// player.position = "Holl";
+// player.departament = "Web deportament";
 
-player.skills = ["JavaScript", "React", "CSS", "HTML"];
-player.expirience = [
-    {company: "NameCompany1", role: "...", years: 2},
-    {company: "NameCompany2", role: "...", years: 3}
+// player.skills = ["JavaScript", "React", "CSS", "HTML"];
+// player.expirience = [
+//     {company: "NameCompany1", role: "...", years: 2},
+//     {company: "NameCompany2", role: "...", years: 3}
+// ]
+
+// player.skills.push("NewSkill");
+// console.log(player);
+
+// delete player.expirience[0];
+// console.log(player);
+
+// for (let key in player) {
+//     console.log(`${key}-------${player[key]}`);
+// }
+
+
+
+// const dictionary = {
+//     'привет':'hello',
+//     'мир':'world',
+//     'дерево': 'tree'
+// }
+
+// function translate(word) {
+//     return dictionary[word];
+// }
+
+// console.log(translate('привет'));
+// console.log(translate('мир'))
+
+const list_dict = [
+    {russian: 'Яблоко', english: 'Apple'},
+    {russian: 'Машина', english: 'Car'}
 ]
 
-player.skills.push("NewSkill");
-console.log(player);
+let current_word = 0
 
-delete player.expirience[0];
-console.log(player);
+document.getElementById("english_word").textContent = list_dict[current_word].english
 
-for (let key in player) {
-    console.log(`${key}-------${player[key]}`);
+function checkTranslation() {
+    let user_input = document.getElementById("userInput").value;
+    if (user_input == list_dict[current_word].russian) {
+        current_word++;
+        document.getElementById("english_word").textContent = list_dict[current_word].english
+    }
 }
-
-
-
-const dictionary = {
-    'привет':'hello',
-    'мир':'world'
-}
-
-function translate(word) {
-    return dictionary[word];
-}
-
-console.log(translate('привет'));
-console.log(translate('мир'))
