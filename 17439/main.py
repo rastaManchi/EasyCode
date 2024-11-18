9c9616583118fda5433cb9ea5f543df25946e17c
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2024-11-18 21:39:17 +0300

    18.11.2024

--- 17439/main.py
+++ 17439/main.py
@@ -1,20 +1,40 @@
-# promo = 'useless'
+spisok = ['молоко', 'яблоки', 'сок', 'орехи', 10000]
+
+# spisok[0:3]
+# spisok[0] = 'газировка'
+
+# Методы добавления!
+# spisok.append(777)
+# spisok.insert(0, 777)
+
+# Методы удаления!
+# spisok.pop(1)
+# spisok.remove('орехи')
+
+# if 'молоко' in spisok:
+#     print('Молоко есть!')
+# else:
+#     print('Молока нет!')
+
+# if 5 < len(spisok):
+#     spisok.pop(5)
+# else:
+#     print('Элемента с индексом 5 в списке нет!')
+
+
+# films = []
 # for i in range(3):
-#     name = input('Введите имя: ')
-#     print(f'{name}, ваш промокод: {promo}')
+#     film_name = input('Введите название фильма: ')
+#     films.append(film_name)
+# print(films)
+# to_delete_film = input('Введите просмотренный фильм: ')
+# if to_delete_film in films:
+#     films.remove(to_delete_film)
+# else:
+#     print('Такой фильм не найден!')
+# print(films)
 
+names = ['Булат', 'Артем', 'Илья']
 
-# for symbol in 'Булат':
-#      print(symbol)
-
-alp = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-step = int(input('Введите шаг: '))
-message = input('Введите сообщение: ')
-encoded = ''
-
-for symbol in message:
-    index = alp.find(symbol)
-    new_index = index - step
-    encoded += alp[new_index]
-
-print(encoded)
+for name in names:
+    print(name)
