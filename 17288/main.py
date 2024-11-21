dacc625079dfec2cfe287f3e0a2649d3fada4971
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2024-11-22 00:53:43 +0300

    22.11.2024

--- 17288/main.py
+++ 17288/main.py
@@ -1,2 +1,24 @@
-for i in 1, 2, 3, 4, 5:
-    print(i)
+# spisok = ['Булат', 22, 'Казань', 'Учитель']
+# spisok[1] = 23
+
+# spisok.append('Закиров_в_конце')
+# spisok.insert(1, 'Закиров_в_начале')
+
+# print(spisok)
+
+# spisok.pop()
+# # del spisok[0]
+# spisok.remove('Казань')
+
+# print(spisok)
+
+# print(len(spisok))
+
+names = ['Булат', 'Максим', 'Павел', 'Артем', 'НОВОЕ', 21312381, 123123.123123]
+
+# for i in range(len(names)):
+#     print(names[i])
+
+
+for name in names:
+    print(name)
