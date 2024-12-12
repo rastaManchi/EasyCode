2ca1593976bd8ff43069dd7fb6451a10c01a34ef
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2024-12-12 22:43:53 +0300

    12.12.2024

--- 17288/main.py
+++ 17288/main.py
@@ -1,9 +1,11 @@
-info = [
-    {
-        'name': 'Булат'
-    },
-    {
-        'name': 'максим'
-    }
-]
-print(info[1]['name'])
+c = 0
+
+
+def summa():
+    global c
+    c += 1
+    print(f'локальная: {c}')
+
+
+summa()
+print(f'Глобальная: {c}')
