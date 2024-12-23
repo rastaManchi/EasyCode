b399ee2f707343d7f829cc7367f58a2717547705
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2024-12-23 19:39:32 +0300

    23.12.2024

--- 17439/step.py
+++ 17439/step.py
@@ -48,29 +48,36 @@ name = input('Введи свое имя, путник: ')
 player['name'] = name
 current_enemy = 0
 
-round = randint(1, 2)
-enemy = enemies[current_enemy]
-enemy_hp = enemy['hp']
 
-print(f'Противник -- {enemy["name"]}: {enemy["script"]}')
-input('Enter чтобы продолжить')
-print()
+while True:
+    action = input('''Выбери действие:
+1 - В бой!
+2 - Тренировка''')
+    if action == '1':
+        round = randint(1, 2)
+        enemy = enemies[current_enemy]
+        enemy_hp = enemy['hp']
 
-while player['hp'] > 0 and enemy_hp > 0:
-    if round % 2 == 1:
-        print(f'{player["name"]} атакует {enemy["name"]}')
-        enemy_hp -= player['attack']
-        sleep(1)
-        print(f'{player["name"]} -- {player["hp"]}\n{enemy["name"]} -- {enemy_hp}')
-        sleep(1)
-    else:
-        print(f'{enemy["name"]} атакует {player["name"]}')
-        player['hp'] -= enemy['attack']
-        sleep(1)
-        print(f'{player["name"]} -- {player["hp"]}\n{enemy["name"]} -- {enemy_hp}')
-        sleep(1)
-    round += 1
-if player['hp'] > 0:
-    print(f'Противник - {enemy["name"]}: {enemy["win"]}')
-else:
-    print(f'Противник - {enemy["name"]}: {enemy["loss"]}')
+        print(f'Противник -- {enemy["name"]}: {enemy["script"]}')
+        input('Enter чтобы продолжить')
+        print()
+
+        while player['hp'] > 0 and enemy_hp > 0:
+            if round % 2 == 1:
+                print(f'{player["name"]} атакует {enemy["name"]}')
+                enemy_hp -= player['attack']
+                sleep(1)
+                print(f'{player["name"]} -- {player["hp"]}\n{enemy["name"]} -- {enemy_hp}')
+                sleep(1)
+            else:
+                print(f'{enemy["name"]} атакует {player["name"]}')
+                player['hp'] -= enemy['attack']
+                sleep(1)
+                print(f'{player["name"]} -- {player["hp"]}\n{enemy["name"]} -- {enemy_hp}')
+                sleep(1)
+            round += 1
+        if player['hp'] > 0:
+            print(f'Противник - {enemy["name"]}: {enemy["win"]}')
+        else:
+            print(f'Противник - {enemy["name"]}: {enemy["loss"]}')
+        player['hp'] = 100
---
+++ 17929/functions.py
@@ -0,0 +1,10 @@
+def length(time, speed):
+    return time*speed
+
+
+def speed(length, time):
+    return length/time
+
+
+def time(length, speed):
+    return length/speed
