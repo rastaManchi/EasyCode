7a4336aff7b7429d54a9e65208110d4b1eb58697
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2025-03-11 23:12:50 +0300

    11.03.2025

--- 27593/OOP.py
+++ 27593/OOP.py
@@ -1,37 +1,65 @@
-class Item:
-    def __init__(self, id, name, description, price):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.price = price
+import pygame # подключаем библиотеку
+import sys # подключаем модуль для 
+from random import *
 
-class Cart:
-    def __init__(self):
-        self.items = []
-        self.total = 0
+pygame.init()
 
-    def addToCart(self, i:Item):
-        self.items.append(i)
-        self.total += i.price
-
-class User:
-    def __init__(self, id, name, balance, cart:Cart):
-        self.id = id
-        self.name = name
-        self.balance = balance
-        self.cart = cart
-
-    def buy(self):
-        if self.balance >= self.cart.total:
-            self.balance -= self.cart.total
-            self.cart.items = []
-            return True
-        return False
+WIDTH = 500
+HEIGHT = 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
 
 
-apple = Item(1, 'Яблоко', 'Описание', 100)
+class Player:
+    def __init__(self, x, y, w, h, img, direction):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.orig_img = pygame.image.load(img)
+        self.img = pygame.transform.scale(self.orig_img, (w, h))
+        self.direction = direction
 
-u1 = User(1, 'Булат', 50, Cart())
-u1.cart.addToCart(apple)
-print(u1.buy(), u1.balance)
+    def move(self):
+        if self.direction == 'right':
+            self.rect.x += 5
+        elif self.direction == 'left':
+            self.rect.x -= 5
+        elif self.direction == 'up':
+            self.rect.y -= 5
+        elif self.direction == 'down':
+            self.rect.y += 5
 
+    def draw(self):
+        screen.blit(self.img, self.rect)
+
+
+player = Player(225, 225, 50, 50, '27593/steve.png', 'none')
+
+
+while True:
+    screen.fill((255, 255, 255))
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            sys.exit()
+        if e.type == pygame.KEYDOWN:
+            if e.key == pygame.K_RIGHT:
+                player.direction = 'right'
+            elif e.key == pygame.K_LEFT:
+                player.direction = 'left'
+            elif e.key == pygame.K_UP:
+                player.direction = 'up'
+            elif e.key == pygame.K_DOWN:
+                player.direction = 'down'
+        if e.type == pygame.KEYUP:
+            player.direction = 'none'
+        if e.type == pygame.MOUSEBUTTONDOWN:
+            x, y = e.pos
+            if player.rect.x <= x <= player.rect.right and \
+                player.rect.y <= y <= player.rect.bottom:
+                player.orig_img = pygame.image.load('27593/SANS.png')
+                player.img = pygame.transform.scale(player.orig_img, (player.rect.width, player.rect.height))
+                
+    
+    player.move()
+    player.draw()
+
+    pygame.display.update()
+    clock.tick(60)
