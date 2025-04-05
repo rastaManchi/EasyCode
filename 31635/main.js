e8ecfcc108f10a6fd4e81923ddaf8b40cd015b06
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2025-04-05 10:44:45 +0300

    05.04.2025

--- 31635/main.js
+++ 31635/main.js
@@ -1,51 +1,27 @@
-// function rectangle_area(width, height) {
-//     let result = width * height
-//     console.log(result)
-// }
-// function calculateTriangleArea(base, height){
-//     return 0.5 * base * height;
-// }
-// function calculateCircleArea(radius) {
-//     return Math.PI * radius * radius;
-// }
-
-
-// rectangle_area(5, 10)
-// rectangle_area(10, 10)
-// let rei_result = calculateTriangleArea(1, 10)
-// let circle_result = calculateCircleArea(5)
-// console.log(rei_result)
-// console.log(circle_result)
-
-
-// console.log(8%2==0)
-
-
-// function ischetnoe(number) {
-//     if (number%2==0) {
-//         return 'четное'
-//     }
-//     else {
-//         return 'нечетное'
+// for (let i = 1; i<=20; i++) {
+//     if (i % 2 == '0') {
+//         console.log(i)
 //     }
 // }
 
-// console.log(ischetnoe(21))
- 
 
-function a(price, sale) {
-    return price*(1-sale/100)
-}
-console.log(a(100, 15))
-
-const input = document.getElementById('calc')
-input.addEventListener('input', function () {
-    alert('Не вводи ')
-})
+// for (let i = 1; i <= 10; i++) {
+//     let row = '';
+//     for (let j = 1; j <= 10; j++) {
+//         row += (i*j) + '\t'
+//     }
+//     console.log(row)
+// }
 
 
-function calculateDiscountedPrice(originalPrice, discountPercentage) {
-    const discountAmount = (originalPrice * discountPercentage) / 100;
-    const discountedPrice = originalPrice - discountAmount;
-    return discountedPrice;
+let width = 10
+let height = 5
+
+
+for (let i = 1; i <= height; i++) {
+    let row = ''
+    for (let j = 1; j <= width; j++) {
+        row += '*'
+    }
+    console.log(row)
 }
