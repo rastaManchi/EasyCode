25501ad621902a8541a55349bf585a919bd0be4a
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2025-07-13 11:28:35 +0300

    13.07.2025

--- 34501/main.py
+++ 34501/main.py
@@ -1,6 +1,6 @@
 import asyncio
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher, F, Router
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InputFile
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
@@ -8,9 +8,13 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.filters import Command
 from helpers import *
-bot = Bot(token='6943333150:AAGA-uXOML1M88KaLtslNoXzF46eJZm7yxU')
+bot = Bot(token='7129279159:AAE-CpSY4mWh1mmJiLxpLcliP0vZpI-96z4')
 dp = Dispatcher(storage=MemoryStorage())
 
+
+class Buy(StatesGroup):
+    name = State()
+    phone = State()
 
 
 
@@ -28,10 +32,33 @@ async def start(message: Message, state: FSMContext):
         'или на кнопку "КУПИТЬ БИЛЕТ", чтобы купить билет.',reply_markup=slides.as_markup(rezise_keydoard=True))
 
 
+@dp.message(F.text, Buy.name)
+async def name_waiting(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer('Приятно познакомиться, введите ваш номер телефона: ')
+    await state.set_state(Buy.phone)
+    
+    
+@dp.message(F.text, Buy.phone)
+async def phone_waiting(message: Message, state: FSMContext):
+    await state.update_data(phone=message.text)
+    await message.answer('Ваши данные переданы менеджеру!')
+    data = await state.get_data()
+    await bot.send_message(190203478, f'Имя: {data.get("name")}\nТелефон: {data.get("phone")}')
+    await state.clear()
+
+
 @dp.message(F.text)
-async def about(message: Message):
-    slide = get_slide_by_name(message.text)
-    print(slide)
+async def about(message: Message, state: FSMContext):
+    if message.text == 'buy ticket':
+        await message.answer('Цена билета 2000р, введите свое имя: ')
+        await state.set_state(Buy.name)
+    else:
+        slide = get_slide_by_name(message.text)
+        if slide:
+            await message.answer(f"Название горки: {message.text}\nПротяженность:{slide[2]}м\n{slide[3]}")
+        else:
+            await message.answer('Такой горки нет!')
 
 
 # Основная функция запуска
