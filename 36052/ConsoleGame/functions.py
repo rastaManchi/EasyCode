de1f7699b6d0caa4605a134120e604eaff8f8a90
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2025-08-17 13:21:16 +0300

    17.08.2025

---
+++ 36052/ConsoleGame/functions.py
@@ -0,0 +1,47 @@
+import time
+import random
+from data import *
+
+def fight(current_enemy):
+    round = random.randint(1, 2)
+    enemy = enemies[current_enemy]
+    enemy_hp = enemies[current_enemy]['hp']
+    print(f'Противник - {enemy["name"]}: {enemy["script"]}')
+    input('Enter, чтобы продолжить')
+    print()
+    while player['hp'] > 0 and enemy_hp > 0:
+        if round % 2 == 1:
+            print(f'{player["name"]} атакует {enemy["name"]}')
+            crit = random.randint(1, 100)
+            if crit < player["luck"]:
+                enemy_hp -= player["attack"] * 3
+            else:
+                enemy_hp -= player["attack"]
+            time.sleep(1)
+        else:
+            print(f'{enemy["name"]} атакует {player["name"]}')
+            player["hp"] -= enemy["attack"] * player['armor']
+            time.sleep(1)
+        print(f'''{player["name"]} -- {player["hp"]}
+{enemy["name"]} -- {enemy_hp}''')
+        print()
+        time.sleep(1)
+        round += 1
+    if player["hp"] > 0:
+        print(f'Противник - {enemy["name"]}: {enemy["win"]}')
+        current_enemy += 1
+    else:
+        print(f'Противник - {enemy["name"]}: {enemy["loss"]}')
+    player["hp"] = 100
+    return current_enemy
+
+def training():
+    training_type = input('Какой вид:\n1 - Сила\n2 - оборона\n')
+    for i in range(0, 101, 10):
+        print(f'тренировка завершена на {i}%')
+        time.sleep(1)
+    if training_type == 1:
+        player['attack'] += 3
+    else:
+        player['armor'] -= 0.05
+    print('Тренировка завершена')
---
+++ 5363/test.js
@@ -0,0 +1,11 @@
+
+
+
+2401
+
+
+3800 - 38р
+response['data']['bidAsk']['bid'][0]['price'] = 38.0 
+
+
+response['data']['bidAsk']['bid'][0]['price']*
