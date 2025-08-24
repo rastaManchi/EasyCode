d1b5c5eab4d1183f3f3baa6ff6784ced1bf215cd
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2025-08-24 17:20:07 +0300

    24.08.2025

---
+++ 40594/24.08.2025.py
@@ -0,0 +1,14 @@
+# names = ['Булат', 'Александр', 'Иван', 'Вадим']
+# mydict = {'key':'value', 'name': 'Булат'}
+
+# for i in mydict:
+#     print(mydict[i])
+
+# for i in range(100, 0, -1):
+#     print(i)
+
+
+pincode = input('Введите пин-код: ')
+while pincode != '0000':
+    pincode = input('Неверный пин-код\nВведите пин-код заново: ')
+
