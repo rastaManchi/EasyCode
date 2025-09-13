4974026fd69345035fdc836d1bf5956906049b7c
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2025-09-13 11:03:08 +0300

    13.09.2025

--- 21981/main.py
+++ 21981/main.py
@@ -1,26 +1,6 @@
-items = {
-    'Яблоко': 80,
-    'ПК': 50000,
-    'ручка': 50
-}
+k = ('Меркурий', 'Венера', 'Земля', 'Марс', 'Юпитер', 'Сатурн', 'Уран', 'Нептун')
 
-items['ПК']
-items['ПК'] = 70000 # изменение значения
-items['new_key'] = 'new_value' # добавление новой пары
-del items['ручка'] # удаление элемента
-
-keys = list(items.keys()) # список только ключей
-values = list(items.values()) # список только значений
-items_ = list(items.items()) # список элементов
-
-# for key, value in items_:
-#     print(key, value)
-
-# spisok = [1, 2, 'Булат', 4, 5]
-# for i in spisok:
-#     print(i)
+planet_index = int(input('Введите номер планеты: ')) - 1 
+print(k[planet_index])
 
 
-# for i in items:
-#     print(i)
-#     print(items[i])
--- 39042/FirstSite/main.py
+++ 39042/FirstSite/main.py
@@ -4,14 +4,23 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def welcome():
-    return "<a href='https://google.com'>Кнопка</a>"
+@app.route('/frogs')
+def frogs():
+   return render_template('home.html', data={
+        'name': 'Лягушки',
+        'title': 'Лягушки',
+        'text': 'Лягушки — общеупотребительное название группы животных из отряда бесхвостых земноводных. В широком смысле термин «лягушка» относится ко всем представителям отряда бесхвостых. В узком смысле это название применяется по отношению к представителям семейства настоящих лягушек. Личинки лягушек называются головастиками.',
+        'img1': 'https://clck.ru/34RZLn',
+        })
 
 
-@app.route("/profile")
-def profile():
-    return "Профиль"
-
+@app.route('/cats')
+def cats():
+   return render_template('home.html', data={
+        'name': 'Кошки',
+        'title': 'Кошки',
+        'text': 'Кошки все прекрасные и пушистые',
+        'img1': 'https://static.tildacdn.com/tild3163-3066-4534-b633-323361313535/image.png',
+        })
 
 app.run()
