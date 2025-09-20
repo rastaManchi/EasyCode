72a67f0665f4824b8dd0add7ae9cc2aa1801aa6d
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2025-09-20 18:19:30 +0300

    20.09.2025

---
+++ 42768/20.09.25.py
@@ -0,0 +1,28 @@
+# a = 'Булат '
+
+# print(a[-1::-1])
+
+# length = len(a)
+# print(f"Длина строчки: {length}")
+
+# print(f"2 + 2 = {2+2}")
+
+# print("1 - В бой\n2 - Тренировка\n3 - Магазин")
+
+# print("""1 - в бой
+# 2 - Тренировки
+# 3 - Магазин""")
+
+a = '    EaSyCoDe     '
+
+
+print(a.upper())
+print(a.lower())
+print(a.strip().capitalize())
+print(a.replace('EaSyCoDe', "ююю"))
+
+
+
+
+
+
