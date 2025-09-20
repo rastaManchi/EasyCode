6131881539fcb2b739ac2e1a5813bb39fbd5dd13
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2025-09-20 10:53:49 +0300

    20.09.2025

--- 39042/FirstSite/main.py
+++ 39042/FirstSite/main.py
@@ -1,7 +1,30 @@
 from flask import Flask, render_template, request
 
 
+curser = {
+    'EUR': 77,
+    'USD': 63,
+    'UZS': 0.00692,
+    'GBP': 98,
+}
+
+
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+   return render_template('register.html', data={'summ': 0})
+
+
+@app.route('/converter', methods=['POST', 'GET'])
+def converter():
+   form_data = request.form
+   if len(form_data) > 0:
+        summ = form_data.get('summ')
+        currency = form_data.get('currency')
+        return render_template('register.html', data={'summ': int(summ) / curser[currency]})
+   return render_template('register.html', data={'summ': 0})
 
 
 @app.route('/frogs')
