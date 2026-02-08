69a555cc578bdaae9e1bb84966dcb1e7d8c453d5
rastaManchi <61435125+rastaManchi@users.noreply.github.com>
2026-02-08 10:59:34 +0300

    08.02.26

---
+++ 46365/08.02.26.py
@@ -0,0 +1,23 @@
+mnozh = {1, 2, 3, 4, 5.0, '123', True} # создание заполненого
+# mnozh = set() # создание пустого множества
+
+mnozh.add(123)
+
+mnozh.remove(1)
+mnozh.discard(100000)
+
+
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+print(a.union(b))
+print(b.union(a))
+
+print(a.intersection(b))
+print(b.intersection(a))
+
+print(a.symmetric_difference(b))
+print(b.symmetric_difference(a))
+
+print(a.difference(b))
+print(b.difference(a))
