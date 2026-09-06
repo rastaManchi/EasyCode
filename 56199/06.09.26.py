6afd02de333673644250a775f7a8a5de33b24114
rastabubblegum <vaca.hack01@gmail.com>
2026-09-06 10:45:26 +0300

    Update

--- 56199/06.09.26.py
+++ 56199/06.09.26.py
@@ -17,12 +17,37 @@ def on_button_press(event):
     start_y = event.y
 
 
+def on_mouse_drag(event):
+    canvas.delete("temp")
+    if current_shape == "line":
+        canvas.create_line(start_x, start_y, event.x, event.y, tags="temp")
+    elif current_shape == "rect":
+        canvas.create_rectangle(start_x, start_y, event.x, event.y, tags="temp")
+    elif current_shape == "circle":
+        canvas.create_oval(start_x, start_y, event.x, event.y, tags="temp")
+
+
+def on_button_release(event):
+    if current_shape == "line":
+        canvas.create_line(start_x, start_y, event.x, event.y)
+    elif current_shape == "rect":
+        canvas.create_rectangle(start_x, start_y, event.x, event.y)
+    elif current_shape == "circle":
+        canvas.create_oval(start_x, start_y, event.x, event.y)
+
+
+def clear_canvas():
+    canvas.delete("all")
+
+
 root = tk.Tk()
 root.title("рисовашка")
 
-canvas = tk.Canvas(root, bg="white", width=800, height=800)
+canvas = tk.Canvas(root, bg="white", width=400, height=400)
 canvas.pack()
 canvas.bind("<Button-1>", on_button_press)
+canvas.bind("<B1-Motion>", on_mouse_drag)
+canvas.bind("<ButtonRelease-1>", on_button_release)
 
 button_frame = tk.Frame(root)
 button_frame.pack()
@@ -36,7 +61,7 @@ rect_btn.pack(side=tk.LEFT)
 circle_btn = tk.Button(button_frame, text="Круг", command=lambda: set_shape("circle"))
 circle_btn.pack(side=tk.LEFT)
 
-clear_btn = tk.Button(button_frame, text="Очистить")
+clear_btn = tk.Button(button_frame, text="Очистить", command=clear_canvas)
 clear_btn.pack(side=tk.LEFT)
 
 
